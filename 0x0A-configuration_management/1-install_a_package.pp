ddd7e557432c32e8798d3979c7e679931a3f8d55
MohamedYehia20m <medoyehia2001@gmail.com>
2024-02-28 18:13:28 +0200

    modfify Task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,6 @@
 # Puppet manifest to install Flask version 2.1.0 using pip3
 
-class { 'python':
-  version => 'system',
-}
-
-class { 'python::pip':
-  ensure  => 'present',
-  version => 'system',
-}
-
 python::pip { 'flask':
   ensure     => '2.1.0',
   provider   => 'pip3',
-}
-
-package { 'python3-flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Python::Pip['flask'],
 }
